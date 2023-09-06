5025a1f056c4712fd5391e757ceba85b75642472
baby-0105 <epifa_kikka@icloud.com>
2023-09-07 00:44:38 +0900

    net keibaのタイトル取得

--- backend/scripts/net_keiba.py
+++ backend/scripts/net_keiba.py
@@ -2,14 +2,22 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
 
-options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument("--headless")
-driver = Chrome(options=options)
-
-driver.get("http://selenium.dev")
-
-driver.quit()
-
 class NetKeibaScraping():
-    pass
+    driver = None
+
+    def __init__(self):
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument("--headless")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = Chrome(options=options)
+        self.driver.get("https://www.netkeiba.com/")
+
+    def get_title(self):
+        title = self.driver.title
+        print(title)
+
+if __name__ == "__main__":
+    net_keiba_scraping = NetKeibaScraping()
+    net_keiba_scraping.get_title()
+    net_keiba_scraping.driver.quit()
